b9fbe14fa665ed307bbfdc08d1ef88c6abce8ae1
pockypoem <jeremyas.cornelis@gmail.com>
2023-07-05 14:00:43 +0700

    LP02 done

--- landingPage02/README.md
+++ landingPage02/README.md
@@ -1,5 +1,8 @@
 # Landing Page
 
+## Demo Link
+https://company-lp02.netlify.app/
+
 ## Tech Stack
 * HTML
 * CSS
@@ -11,3 +14,5 @@
 * https://cdnjs.com/libraries/font-awesome/6.1.2
 * https://remixicon.com/
 * https://github.com/lokesh/lightbox2
+* https://animate.style/
+* https://michalsnik.github.io/aos/
--- landingPage02/dist/js/script.js
+++ landingPage02/dist/js/script.js
@@ -18,3 +18,32 @@ const iconClose = document.querySelector('.ri-close-line');
 iconClose.addEventListener('click', () => {
     sideBar.classList.remove('menu-active');
 });
+
+// Swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
+
+
+// AOS
+AOS.init();     
